4df2383f0605ae1f961b4e3bc7d7820686ee96bd
thengo1 <tbn2109@columbia.edu>
2021-07-22 05:22:25 -0400

    Adding user auth and encryption

--- models/User.js
+++ models/User.js
@@ -1,42 +1,46 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        min: 3,
-        max: 16,
-        unique: true
+        username: {
+            type: String,
+            required: true,
+            min: 3,
+            max: 16,
+            unique: true
+        },
+        email: {
+            type: String,
+            required: true,
+            max: 50,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            min: 6,
+        },
+        profilePicture: {
+            type: String,
+            default: "",
+        },
+        coverPicture: {
+            type: String,
+            default: "",
+        },
+        followers: {
+            type: Array,
+            default: [],
+        },
+        following: {
+            type: Array,
+            default: [],
+        },
+        isAdmin: {
+            type: Boolean,
+            default: false,
+        },
     },
-    email: {
-        type: String,
-        required: true,
-        max: 50,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-        min: 6,
-    },
-    profilePicture: {
-        type: String,
-        default: "",
-    },
-    coverPicture: {
-        type: String,
-        default: "",
-    },
-    followers: {
-        type: Array,
-        default: [],
-    },
-    following: {
-        type: Array,
-        default: [],
-    },
-    isAdmin: {
-        type: Boolean,
-        default: false,
-    },
-});
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,27 @@
 const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
-router.get("/", (req, res) => {
-    res.send("Auth routes")
-})
+// Register user
+router.post("/register", async  (req, res) => {
+    try {
+        // Generate new encrypted password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+        // Create new user
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword,
+        });
+
+        // Save user in DB and return response
+        const user = await newUser.save();
+        res.status(200).json(user);
+    } catch(err) {
+        console.log(err);
+    }
+});
 
 module.exports = router;
